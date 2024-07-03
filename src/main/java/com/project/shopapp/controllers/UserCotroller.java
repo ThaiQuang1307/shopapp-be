1ca1fd6452ebbf5689f999d7ca1da47caf4e063c
ThaiQuang1307 <quangmath1307@gmail.com>
2024-07-03 17:35:23 +0700

    update function

--- src/main/java/com/project/shopapp/controllers/UserCotroller.java
+++ src/main/java/com/project/shopapp/controllers/UserCotroller.java
@@ -3,9 +3,12 @@ package com.project.shopapp.controllers;
 import com.project.shopapp.dtos.UserDTO;
 import com.project.shopapp.dtos.UserLoginDTO;
 import com.project.shopapp.models.UserModel;
+import com.project.shopapp.responses.LoginResponse;
 import com.project.shopapp.services.IUserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -14,14 +17,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.LocaleResolver;
 
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("${api.prefix}/users")
 @RequiredArgsConstructor
 public class UserCotroller {
     private final IUserService userService;
+    private final MessageSource messageSource;
+    private final LocaleResolver localeResolver;
 
     // Đăng ký
     @PostMapping("/register")
@@ -49,13 +56,21 @@ public class UserCotroller {
 
     // đăng nhập
     @PostMapping("/login")
-    public ResponseEntity<String> login(
-            @Valid @RequestBody UserLoginDTO userLoginDTO
+    public ResponseEntity<?> login(
+            @Valid @RequestBody UserLoginDTO userLoginDTO,
+            HttpServletRequest request
     ) {
         try {
             String token = userService.login(userLoginDTO.getPhoneNumber(), userLoginDTO.getPassword());
-            return ResponseEntity.ok(token);
-        } catch (Exception e){
+            Locale locale = localeResolver.resolveLocale(request);
+            return ResponseEntity.ok(LoginResponse
+                    .builder()
+                    .message(messageSource.getMessage("user.login.login_successfully", null, locale))
+                    .token(token)
+                    .build()
+            );
+//            return ResponseEntity.ok(token);
+        } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
---
+++ src/main/java/com/project/shopapp/responses/LoginResponse.java
@@ -0,0 +1,17 @@
+package com.project.shopapp.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("token")
+    private String token;
+}
